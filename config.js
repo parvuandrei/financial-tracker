678da0787e700cc989f86fd05fdcffe50b0cb7a4
parvuandrei <andrei.i.parvu@gmail.com>
2026-09-25 00:16:12 +0300

    Connect Supabase project

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Public browser configuration. Never put a secret/service-role key here.
 window.FINTRACK_CONFIG = Object.freeze({
-  supabaseUrl: '',
-  supabasePublishableKey: ''
+  supabaseUrl: 'https://xpwdghfdhyrirkybeakw.supabase.co',
+  supabasePublishableKey: 'sb_publishable_rbGj9LEJO_s0RWEbCEjzww_SKaIUeiY'
 });
